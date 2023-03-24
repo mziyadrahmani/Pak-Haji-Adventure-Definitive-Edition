85143d313369ce0a29d70a993766f38edcd1be7c
DreamNinjas <mziyadrgt@gmail.com>
2023-03-24 17:08:21 +0800

    17:00 Friday

--- Main Menu/MainMenu.gd
+++ Main Menu/MainMenu.gd
@@ -1,11 +1,29 @@
 extends Control
-
+@onready var checksave = Global.get("isgamesaved")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$ColorRect2.visible = true
 	$AnimationPlayer.play("Transition/DipIn")
 	await get_tree().create_timer(3).timeout
 	$ColorRect2.visible = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_exit_button_button_up():
+	$ColorRect2.visible = true
+	$"AnimationPlayer".play("Transition/DipOut")
+	await get_tree().create_timer(3).timeout
+	get_tree().quit()
+
+
+func _on_play_button_button_up():
+	if checksave == true :
+		$ColorRect2.visible = true
+		$"AnimationPlayer".play("Transition/DipOut")
+		await get_tree().create_timer(3).timeout
+		get_tree().change_scene_to_file("res://Main Menu/LoadGame.tscn")
+	else : 
+		pass
--- Main Menu/Splash.gd
+++ Main Menu/Splash.gd
@@ -8,7 +8,10 @@ func _ready():
 	$AnimationPlayer.play("DipOut")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://Main Menu/MainMenu.tscn")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
