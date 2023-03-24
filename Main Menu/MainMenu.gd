ecfc8141c524e2c7bb2cd0eb91e859e020e5260f
DreamNinjas <mziyadrgt@gmail.com>
2023-03-25 00:09:47 +0800

    Saturday 25-3-2023
    Tiles

--- Main Menu/MainMenu.gd
+++ Main Menu/MainMenu.gd
@@ -7,9 +7,7 @@ func _ready():
 	$AnimationPlayer.play("Transition/DipIn")
 	await get_tree().create_timer(3).timeout
 	$ColorRect2.visible = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+
 
 
 func _on_exit_button_button_up():
@@ -23,7 +21,11 @@ func _on_play_button_button_up():
 	if checksave == true :
 		$ColorRect2.visible = true
 		$"AnimationPlayer".play("Transition/DipOut")
-		await get_tree().create_timer(3).timeout
+		await get_tree().create_timer(3.1).timeout
 		get_tree().change_scene_to_file("res://Main Menu/LoadGame.tscn")
+		
 	else : 
-		pass
+		$ColorRect2.visible = true
+		$"AnimationPlayer".play("Transition/DipOut")
+		await get_tree().create_timer(3.1).timeout
+		get_tree().change_scene_to_file("res://World/World1.tscn")
--- Main Menu/Splash.gd
+++ Main Menu/Splash.gd
@@ -6,12 +6,8 @@ func _ready():
 	$AnimationPlayer.play("DipIn")
 	await get_tree().create_timer(3).timeout
 	$AnimationPlayer.play("DipOut")
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(3.1).timeout
 	get_tree().change_scene_to_file("res://Main Menu/MainMenu.tscn")
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
 
