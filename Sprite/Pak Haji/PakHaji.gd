ff1d060a124ca9aca8104f6470cec6ae496e09ea
DreamNinjas <mziyadrgt@gmail.com>
2023-03-30 23:55:29 +0800

    fixed script and sprite
    holymoly

--- Sprite/Pak Haji/PakHaji.gd
+++ Sprite/Pak Haji/PakHaji.gd
@@ -18,19 +18,11 @@ func _ready():
 func _physics_process(delta):
 	#print(crntcoor)
 	if (velocity.x <0):
-			animsprite.flip_h = true
+		animsprite.flip_h = true
+		
 	elif (velocity.x >0):
 		animsprite.flip_h = false
-	if isclimbing == true and is_on_wall() :
-		#print("climb")
-		var updowndir = Input.get_axis("jump", "down")
-		if updowndir :
-			velocity.y = updowndir * SPEED
-			animsprite.speed_scale = abs(velocity.y) * 0.01
-			animsprite.play("climb")
-		else:
-			velocity.y = move_toward(velocity.x, 0, SPEED)
-			animsprite.play("climbidle")
+	
 	if is_on_wall() :
 		for i in get_slide_collision_count():
 			var collision = get_slide_collision(i)
@@ -38,27 +30,24 @@ func _physics_process(delta):
 			#print(object)
 			if object.name == "Climbable" and object.is_in_group("climbable"):
 				isclimbing = true
+				climbing()
 			else :
+				#print("isnotonclimbable")
 				isclimbing = false
-	if not is_on_floor() and not isclimbing:
+	if not is_on_floor() and not is_on_wall():
 		velocity.y += gravity * delta
 		
 		if (velocity.y <= 0):
 			animsprite.play("jump")
-			#print("jumping")
 		if (velocity.y > 0):
 			animsprite.play("fall")
-			#print("falling")
 	else :
 		if (velocity.x == 0) and not isclimbing:
 			animsprite.play("idle")
-			#print("idling")
 		elif (velocity.x != 0) and not isclimbing: 
 			animsprite.speed_scale = abs(velocity.x) * 0.01
 			animsprite.play("walk")
 			
-			#print("walking")
-	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		var tween = create_tween()
@@ -68,15 +57,10 @@ func _physics_process(delta):
 		tween.tween_property(animsprite,"scale",Vector2(1,1),0.2)
 		
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction and not isclimbing:
 		velocity.x = direction * SPEED
-		
-		
-		
-		
+
 	elif direction and isclimbing :	
 		velocity.x = direction * SPEED
 		velocity.y += gravity * delta
@@ -86,4 +70,19 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func climbing():
+	if isclimbing == true and is_on_wall() :
+		#print("climb")
+		var updowndir = Input.get_axis("jump", "down")
+		if updowndir :
+			velocity.y = updowndir * SPEED
+			animsprite.speed_scale = abs(velocity.y) * 0.01
+			animsprite.play("climb")
+			
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+			animsprite.play("climbidle")
+			#print("isnotclimbing")
+
 
